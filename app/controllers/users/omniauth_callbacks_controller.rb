ffef555d13baa29c2e3ea32a5f9a7176be2a787f
amadden80 <amadden80@gmail.com>
2013-07-09 17:07:05 -0400

    Twitter Integrate

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
       p request.env['omniauth.auth']
+      @user = User.find_for_twitter_oauth(request.env['omniauth.auth'], current_user)
+      if @user.persisted?
+        flash[:notice] = 'Successfully connected to Twitter'
+      else
+        flash[:notice] = 'Unable connect to Twitter'
+      end
+
+      redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,34 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :identities
+
+  def self.find_for_twitter_oauth(auth, user)
+    identity = user.identities.where(provider: auth.provider).first
+    if identity
+      identity.update_attributes(name: auth.extra.raw_info.name,
+                                 user_name: auth.extra.raw_info.screen_name,
+                                 provider: auth.provider,
+                                 uid: auth.uid,
+                                 url: auth.info.urls.Twitter,
+                                 image: auth.extra.raw_info.profile_image_url_https,
+                                 token: auth.extra.access_token.token,
+                                 secret: auth.extra.access_token.secret)
+    else
+      user.identities << Identity.create(
+      name: auth.extra.raw_info.name,
+                                 user_name: auth.extra.raw_info.screen_name,
+                                 provider: auth.provider,
+                                 uid: auth.uid,
+                                 url: auth.info.urls.Twitter,
+                                 image: auth.extra.raw_info.profile_image_url_https,
+                                 token: auth.extra.access_token.token,
+                                 secret: auth.extra.access_token.secret,
+                                 user_id: user.id)
+      user.save!
+    end
+
+    user
+  end
 end
