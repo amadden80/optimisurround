9a6ac7c6df47b1e9c2a14f9f7735ea8f48ddb085
amadden80 <amadden80@gmail.com>
2013-07-12 17:11:36 -0400

    Sciss, Hair

--- app/assets/javascripts/virtualhair_cut.js
+++ app/assets/javascripts/virtualhair_cut.js
@@ -1,25 +1,61 @@
 
 
 var Global_Data = {}
+var sciss_node = $('<img src ="/open-sciss.png" />')
+
+
+function get_hair(){
+  var hair_name = '/hair/hair_'+Math.floor((Math.random()*9)+1) + '.png';
+  hair_node = $('<img src ="'+hair_name+'">')
+  $(hair_node).css('position', 'absolute')
+  return hair_node
+}
+
+
+function cut_animate(){
+  $(sciss_node).css('position', 'absolute')
+  $(sciss_node).css('width', '3%')
+  $(sciss_node).css('top', Global_Data.y_page-25);
+  $(sciss_node).css('left', Global_Data.x_page-25);
+  $(sciss_node).show()
+}
 
 
 $(function(){
 
-$("#cutting-pad").click(function(e){
-    Global_Data.x_percnt = parseFloat(e.offsetX)/$("#cutting-pad").width();
-    Global_Data.y_percnt = parseFloat(e.offsetY)/$("#cutting-pad").height();
-  })
+  $(sciss_node).hide()
+  $("#pad-box").append(sciss_node)
+
+
+  $("#cutting-pad").click(function(e){
+      Global_Data.x_percnt = parseFloat(e.offsetX)/$("#cutting-pad").width();
+      Global_Data.y_percnt = parseFloat(e.offsetY)/$("#cutting-pad").height();
+      Global_Data.x_page = parseFloat(e.pageX);
+      Global_Data.y_page = parseFloat(e.pageY);
+    })
 
   $("#cutting-pad").on('click', function(pd){
-
+    
+    cut_animate()
     $.ajax({
     type: "POST",
     url: "/sciss",
     data: Global_Data,
     dataType:"script"
-    })
-
+    }).done(function(){
+      var hair_node = get_hair()
+      $(hair_node).css('width', '1%')
+      $(hair_node).css('top', Global_Data.y_page-10);
+      $(hair_node).css('left', Global_Data.x_page);
+      $("#pad-box").append(hair_node)
+      sciss_node.attr('src', "/closed-sciss.png")
+      $(hair_node).animate({top: '+=100'}, 1000, function(){
+        $(hair_node).fadeOut(100)
+        sciss_node.attr('src', "/open-sciss.png")
+      })
+      })
   })
-  
+
+
 
 })
