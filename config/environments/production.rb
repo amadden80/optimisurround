15025c3646fdf61ca9229ae4cc25dc6d2662696f
amadden80 <amadden80@gmail.com>
2013-07-13 17:26:53 -0400

    Visualizeingzz2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,21 @@ Optimissurround::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+
+
+  config.action_mailer.default_url_options = { :host => 'Optimissurround.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :authentication => 'plain',
+    :address => 'smtp.mailgun.org',
+    :port => 587,
+    :user_name => 'postmaster@app15937760.mailgun.org',
+    :password => '4t4ulih73el4',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 end
