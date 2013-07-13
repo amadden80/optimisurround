8cac8d6124e01c55da6aa88b660e7f180a158160
amadden80 <amadden80@gmail.com>
2013-07-13 17:55:56 -0400

    Resize vis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,6 @@ Optimissurround::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 
-
-
   config.action_mailer.default_url_options = { :host => 'Optimissurround.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
